923e51e557447c1fe1ca33e13d2d8e1598125415
Sophia YF Ng <ng.yf.sophia@gmail.com>
2020-03-29 19:09:13 +0800

    Updated

--- Testing1.py
+++ Testing1.py
@@ -4,13 +4,14 @@
 import telegram
 import logging
 
-from telegram.ext import Updater, CommandHandler, Message Handler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # token value refers to the API token of your bot
 bot = telegram.Bot(token='1047383488:AAHJVUq0okWEBoPktfnR68bkE96mRcrTysA')
 
 # to test whether your bot is running. Insert your bot display name and username 
-print(bot.get_me()) {"first_name": "TrainingBot", "username": "tbica19_bot"}
+print(bot.get_me()) 
+{"first_name": "TrainingBot", "username": "tbica19_bot"}
 
 updater = Updater(token='1047383488:AAHJVUq0okWEBoPktfnR68bkE96mRcrTysA', use_context=True)
 dispatcher = updater.dispatcher
@@ -18,7 +19,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 # List of Commands for the Bot when you type /cmd to the bot
 def start(update, context):
-context.bot.send_message(chat_id=update.effective_chat.id, text="I am a bot that tries my best to answer your queries")
-start_handler = CommandHandler('start', start) dispatcher.add_handler(start_handler)
-
-def how(update, context):
+	context.bot.send_message(chat_id=update.effective_chat.id, text="I am a bot that tries my best to answer your queries")
+start_handler = CommandHandler('start', start) 
+dispatcher.add_handler(start_handler)
