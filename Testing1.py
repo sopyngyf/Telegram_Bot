3ffb88c7866d573b4fff6b2e29ec67207f1761c3
Sophia YF Ng <ng.yf.sophia@gmail.com>
2020-03-31 23:04:05 +0800

    Update Testing1. py

--- Testing1.py
+++ Testing1.py
@@ -19,8 +19,39 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 # List of Commands for the Bot when you type /cmd to the bot
 def start(update, context):
-	context.bot.send_message(chat_id=update.effective_chat.id, text="I am a bot that tries my best to answer your queries")
+	context.bot.send_message(chat_id=update.effective_chat.id,
+		text="I am a bot that tries my best to answer your queries")
 start_handler = CommandHandler('start', start) 
 dispatcher.add_handler(start_handler)
 
-#xxxx
+def how(update, context):
+	context.bot.send_message(chat_id=update.effective_chat.id,
+		text="type / followed by keyword, for example, /about")
+how_handler = CommandHandler('how', how)
+dispatcher.add_handler(how_handler)
+
+def about(update, context):
+	context.bot.send_message(chat_id=update.effective_chat.id,
+		text="This is a bot to answer your query")
+about_handler = CommandHandler('about', about)
+dispatcher.add_handler(about_handler)
+
+def mc(update, context):
+	context.bot.send_message(chat_id=update.effective_chat.id,
+		text="Submit MC by the following day deployed at HTA. Apply via the PaC@Gov app also")
+mc_handler = CommandHandler('mc', mc)
+dispatcher.add_handler(mc_handler)
+
+def dutyphone(update, context):
+	context.bot.send_message(chat_id=update.effective_chat.id,
+		text="Contact the Duty phone at 9299 2462")
+dutyphone_handler = CommandHandler('dutyphone', dutyphone)
+dispatcher.add_handler(dutyphone_handler)
+
+#Start Bot
+updater.start_polling()
+
+#Stop Bot
+updater.stop()
+
+#End of Code
