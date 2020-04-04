c0e3e37e1a818d3f4c83d9b12f3fae449d7fc0ad
Sophia YF Ng <ng.yf.sophia@gmail.com>
2020-04-04 20:14:01 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# API token for my Telegram Bot
-# .gitignore should include reference to config.py
-
-# API token of the bot
-access_token = "1047383488:AAHJVUq0okWEBoPktfnR68bkE96mRcrTysA"
-
-# End of the API Token
