3458be4b741e60f3c91d2e1db696df1536f0d6ec
Sophia YF Ng <ng.yf.sophia@gmail.com>
2020-04-04 20:00:37 +0800

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -1,8 +1,3 @@
 # List of Commands for the Bot when you type /cmd to the bot
 # Still very new to Python
 
-from telegram.ext import Updater
-from telegram import InilneKeyboardButton, InlineKeyboardMarkup
-from telegram.utils.helpers import escapt_html, escape_markdown
-from sumy.parsers.html import HtmlParser
-from sumy.parsers.plaintext import PlaintextParser
